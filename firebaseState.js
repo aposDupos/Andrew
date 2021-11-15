84f71411107464fbdb767f054e69b6cf3f398c73
Vlad <44709282+aposDupos@users.noreply.github.com>
2021-11-15 23:00:28 +0300

    Add files via upload

---
+++ firebaseState.js
@@ -0,0 +1,44 @@
+import React, {useReducer} from "react";
+import {firebaseReducer} from "./firebaseReducer";
+import {FirebaseContext} from "./firebaseContext";
+import axios from "axios"
+import {BUY_WISH, CHANGE_BALANCE, GET_MONEY, SHOW_LOADER} from "../types";
+
+const url = "https://andrews-wishlist-default-rtdb.europe-west1.firebasedatabase.app"
+export const FirebaseState = ({children}) => {
+    const initialState = {
+        balance: '',
+        wishes: [],
+        loading: false
+    }
+    const [state, dispatch] = useReducer(firebaseReducer, initialState)
+    const showLoader = () => dispatch({type: SHOW_LOADER})
+    const getMoney = async () => {
+        showLoader()
+        const result = await axios.get(`${url}/balance.json`)
+        const payload = result.data
+        dispatch({type: GET_MONEY, payload})
+    }
+    const changeBalance = money => {
+        axios.put(`${url}/balance.json`, state.balance + money)
+        dispatch({type: CHANGE_BALANCE, payload: money})
+    }
+    const buyWish = async title => {
+        axios.post(`${url}/wishes.json`, {title})
+        dispatch({type: BUY_WISH, payload: title})
+    }
+
+
+    return (
+        <FirebaseContext.Provider value={{
+            showLoader,
+            buyWish,
+            changeBalance,
+            getMoney,
+            balance: state.balance,
+            loading: state.loading
+        }}>
+            {children}
+        </FirebaseContext.Provider>
+    )
+}
